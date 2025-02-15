4b9e0bb55bd5c2d131a8f1f7778dd81065fa63ba
ArtemisByte <197433795+ArtemisByte@users.noreply.github.com>
2025-02-15 07:42:01 +0000

    Create cors.ts
    Emergency Addition

---
+++ shared/cors.ts
@@ -0,0 +1,5 @@
+// cors.ts
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': 'https://https://artemisbyte.github.io/RunwaySlots',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+};
