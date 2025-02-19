dc879034aa3f201c7e6ce72b0595ba6e39ceffc3
ArtemisByte <197433795+ArtemisByte@users.noreply.github.com>
2025-02-19 12:52:32 +0000

    Update airport-search.js

--- scripts/airport-search.js
+++ scripts/airport-search.js
@@ -1,16 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import firebaseConfig from "./config/firebaseConfig.js"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAjJ5scHda4eRM-6A-2AfHdFFncudmxiXA",
-  authDomain: "airportinfo-9e5ba.firebaseapp.com",
-  databaseURL: "https://airportinfo-9e5ba-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "airportinfo-9e5ba",
-  storageBucket: "airportinfo-9e5ba.firebasestorage.app",
-  messagingSenderId: "788689422638",
-  appId: "1:788689422638:web:368b08549b15581e15d2bf"
-};
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
