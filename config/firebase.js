9bfbe9c5cef450218159ae5c5af9bf5df38360d2
ArtemisByte <197433795+ArtemisByte@users.noreply.github.com>
2025-02-19 12:50:39 +0000

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,1 +1,13 @@
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAjJ5scHda4eRM-6A-2AfHdFFncudmxiXA",
+  authDomain: "airportinfo-9e5ba.firebaseapp.com",
+  databaseURL: "https://airportinfo-9e5ba-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "airportinfo-9e5ba",
+  storageBucket: "airportinfo-9e5ba.firebasestorage.app",
+  messagingSenderId: "788689422638",
+  appId: "1:788689422638:web:368b08549b15581e15d2bf"
+};
+
+export default firebaseConfig;
 
