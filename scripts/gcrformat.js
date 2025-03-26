a6cc14fa7b32c24bee2988c0c97ac4da40b1897a
ArtemisByte <197433795+ArtemisByte@users.noreply.github.com>
2025-03-26 19:21:23 +0000

    Update gcrformat.js
    Now fixed output,
    error due to theme

--- scripts/gcrformat.js
+++ scripts/gcrformat.js
@@ -1,28 +1,5 @@
-    const themeToggle = document.getElementById('themeToggle');
-    const body = document.body;
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    function applyTheme(theme) {
-      if (theme === 'dark') {
-        body.classList.add('dark-mode');
-        themeToggle.checked = true;
-      } else {
-        body.classList.remove('dark-mode');
-        themeToggle.checked = false;
-      }
-    }
-    applyTheme(savedTheme);
-    themeToggle.addEventListener('change', () => {
-      if (themeToggle.checked) {
-        localStorage.setItem('theme', 'dark');
-        applyTheme('dark');
-      } else {
-        localStorage.setItem('theme', 'light');
-        applyTheme('light');
-      }
-    });
-
     /************************************************
-     CORE LOGIC
+     IATA TO ICAO 
     ************************************************/
     const iataToIcao = {
       STN: "EGSS",
